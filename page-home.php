14ca8a34434a508b30d15163cf66761ba1062e76
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-01-17 02:21:00 +0700

    Add full home background and navigation

---
+++ page-home.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="full-background">
+		<div class="large-6">
+			<h1>Lupakan perbedaan, satukan tujuan. Saatnya kita mulai politik kebangsaan.</h1>
+			<h2>Mari bergabung!</h2>
+			<div class="mailchimp">
+				<!-- Begin MailChimp Signup Form -->
+				<div id="mc_embed_signup">
+				<form action="//langgar.us10.list-manage.com/subscribe/post?u=d3154a05de067b1349804592a&amp;id=af2efb6680" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+					
+				<div class="mc-field-group">
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Alamat Email">
+				</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_d3154a05de067b1349804592a_af2efb6680" tabindex="-1" value=""></div>
+				    <div class="clear"><input type="submit" value="Bergabung" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				    </div>
+				</form>
+				</div>
+
+				<!--End mc_embed_signup-->
+			</div>
+		</div>
+		<div class="large-6">
+			&nbsp;
+		</div>
+</section>
+
+<nav class="topbar">
+	<ul>
+		<li>Profil</li>
+		<li>Berita</li>
+		<li>Galeri</li>
+		<li class="logo"><img src="<?php echo get_template_directory_uri(); ?>/img/zul_logo.svg" alt=""></li>
+		<li>Kalender Kegiatan</li>
+		<li>Dukung Zul</li>
+		<li>Kontak</li>
+	</ul>
+</nav>
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/latest.css">
