64b94e162daed1dac2948216654959346ff0b803
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-01-19 22:21:30 +0700

    Fix mobile layout on relawan page

--- page-daftar-relawan.php
+++ page-daftar-relawan.php
@@ -6,9 +6,9 @@ Template Name: Daftar Relawan
 
 <?php get_header(); ?>
 
-<section class="full-background daftar-relawan">
+<section class="daftar-relawan-full">
 	<div class="row">
-		<div class="large-6 medium-6 columns">
+		<div class="large-6 medium-6 small-12 columns">
 			<h1><em>Lupakan perbedaan, satukan tujuan. Saatnya kita mulai politik kebangsaan.</em></h1>
 			<h2><strong><em>Daftar menjadi Relawan Zulkifli Hasan</em></strong></h2>
 			<?php /* Start loop */ ?>
@@ -18,12 +18,12 @@ Template Name: Daftar Relawan
 				</div>
 			<?php endwhile; // End the loop ?>
 		</div>		
-		<div class="large-6 medium-6 columns">
+		<div class="large-6 medium-6 small-12 columns">
 			&nbsp;
 		</div>
 	</div>
 </section>
-<nav class="topbar daftar-relawan">
+<nav class="daftar-relawan">
 	<div class="large-12 medium-6 columns">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/zul_logo.png" alt=""></li>
 	</div>
