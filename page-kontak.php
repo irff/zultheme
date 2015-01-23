02db42e531e018b9fe6504a0dc9918293a6f7744
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-01-23 15:38:50 +0700

    Add profil/kontak

---
+++ page-kontak.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Kontak
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<div class="header berita">
+	<div class="row">
+		<div class="large-12 columns">
+			<div class="title">		
+				<h1>Kontak</h1>
+			</div>			
+		</div>
+	</div>
+</div>
+
+<?php include('navigation.php'); ?>
+
+<div class="profil">
+	<div class="row">
+		<div class="large-12 medium-12">
+			<article>
+				<p>Suarakan sekarang, bukan esok, atau lusa. Suarakan langsung kepada Zul!</p>
+				<p>Twitter: <a href="https://twitter.com/ZUL_Hasan">@ZUL_Hasan</a></p>
+				<p>Facebook: <a href="https://www.facebook.com/pages/Zulkifli-Hasan/1547369918837496">Zulkifli Hasan</a></p>
+			</article>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
---
+++ page-profil.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Profil
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<div class="header berita">
+	<div class="row">
+		<div class="large-12 columns">
+			<div class="title">		
+				<h1>Profil Zulkifli Hasan</h1>
+			</div>			
+		</div>
+	</div>
+</div>
+
+<?php include('navigation.php'); ?>
+
+<div class="profil">
+	<div class="row">
+		<div class="large-12 medium-12">
+			<article>
+				<p>Dr. (HC). H. Zulkifli Hasan, S.E., M.M. (lahir di Lampung, 17 Mei 1962; umur 52 tahun) adalah politikus yang menjabat Ketua Majelis Permusyawaratan Rakyat Republik Indonesia (MPR RI) menggantikan Sidarto Danusubroto sejak 8 Oktober 2014 untuk periode 2014-2019. Sebelumnya, ia menjabat sebagai Menteri Kehutanan Republik Indonesia menggantikan Malem Sambat Kaban, yaitu sejak 22 Oktober 2009 hingga 1 Oktober 2014. Pada masa jabatannya sebagai MENHUT RI, ia telah dua kali menerima gelar Doktor Kehormatan (Honoris Causa), yaitu yang pertama untuk bidang Administrasi Publik dari Sejong University (Seoul, Korea) dan yang kedua untuk bidang Manajemen Sumber Daya Manusia (SDM) dari Universitas Negeri Semarang (Semarang, Indonesia). Selama tahun 2004-2009, ia berkiprah di lembaga legislatif sebagai anggota Dewan Perwakilan Rakyat Republik Indonesia (DPR RI) dan selama tahun 2005-2010, ia memegang jabatan internal partai sebagai Sekretaris Jenderal Partai Amanat Nasional (PAN).</p>
+				
+				<p>Zulkifli Hasan menyelesaikan pendidikan menengah atas di SMA Negeri 53 Jakarta pada tahun 1982. Selanjutnya, ia mengambil program sarjana di Fakultas Ekonomi Universitas Krisnadwipayana dan mendapat gelar Sarjana Ekonomi pada tahun 1996. Ia kemudian mengambil program pasca sarjana di Sekolah Tinggi Manajemen PPM dan mendapat gelar Magister Manajemen pada tahun 2003. Di awal masa jabatannya sebagai Ketua MPR RI, ia mengemban tugas menyelenggarakan pelantikan presiden-wakil presiden untuk masa pemerintahan 2014-2019 di tengah situasi politik yang penuh ketegangan. Dalam persiapan jelang pelantikan tersebut, dirinya sangat berkomitmen untuk dapat menyelenggarakan pelantikan dengan baik dan akan mengupayakan kehadiran semua pihak, termasuk Prabowo Subianto dan Hatta Rajasa selaku calon presiden dan calon wakil presiden yang menjadi pesaing pasangan pemenang, Joko Widodo dan Jusuf Kalla. Ia berhasil melaksanakan tugasnya dan menciptakan situasi yang damai pada saat pelantikan tersebut belangsung.</p>
+			</article>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
