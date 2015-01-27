33f4add2822b07dd5ed45799ac8f80573a95beef
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-01-27 22:34:56 +0700

    Fix responsive navigation

--- page-daftar-relawan.php
+++ page-daftar-relawan.php
@@ -23,20 +23,21 @@ Template Name: Daftar Relawan
 		</div>
 	</div>
 </section>
-<nav class="daftar-relawan">
-	<div class="large-5 columns">
+
+<nav class="daftar-relawan"> 
+	<div class="large-5 medium-5 columns">
 		<ul class="left-nav">
 			<li><a href="<?php echo esc_url(home_url('/profil')); ?>">Profil</a></li>
 			<li><a href="<?php echo esc_url(home_url('/berita')); ?>">Berita</a></li>
 			<li><a href="<?php echo esc_url(home_url('/galeri')); ?>">Galeri</a></li>			
 		</ul>
 	</div>
-	<div class="large-2 columns">
+	<div class="large-2 medium-2 columns">
 			<a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/zul_logo.png" alt=""></a>
 	</div>
-	<div class="large-5 columns">
+	<div class="large-5 medium-5 columns">
 		<ul class="right-nav">
-			<li><a href="<?php echo esc_url(home_url('/kalender')); ?>">Kalender Kegiatan</a></li>
+			<li><a href="<?php echo esc_url(home_url('/kalender')); ?>">Kalender</a></li>
 			<li><a href="<?php echo esc_url(home_url('/dukung')); ?>">Dukung Zul</a></li>
 			<li><a href="<?php echo esc_url(home_url('/kontak')); ?>">Kontak</a></li>
 		</ul>		
