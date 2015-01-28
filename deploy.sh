f10725033a07147ef25de30eafcffe68657013f3
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-01-28 13:45:18 +0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ssh irfan@104.236.129.7 <<EOF
+  cd ~/zultheme
   git pull
   sudo rm -rf /var/www/zulhasan/wp-content/themes/zultheme/
   sudo mkdir /var/www/zulhasan/wp-content/themes/zultheme
