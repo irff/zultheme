ab213bc1387d9a2dac8c09292951944771840014
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-02-05 01:03:27 +0700

    Fix page header title

--- page.php
+++ page.php
@@ -6,33 +6,33 @@ Template Name: Page Standard
 
 <?php get_header(); ?>
 
+<?php /* Start loop */ ?>
+<?php while (have_posts()) : the_post(); ?>
 
-<div class="header berita">
-	<div class="row">
-		<div class="large-12 columns">
-			<div class="title">		
-				<h1>Kontak</h1>
-			</div>			
+	<div class="header berita">
+		<div class="row">
+			<div class="large-12 columns">
+				<div class="title">		
+					<h1><?php the_title(); ?></h1>
+				</div>			
+			</div>
 		</div>
 	</div>
-</div>
 
-<?php include('navigation.php'); ?>
+	<?php include('navigation.php'); ?>
 
-<div class="profil">
-	<div class="row">
-		<div class="large-12 medium-12" role="main">
-			<article>
-				<?php /* Start loop */ ?>
-				<?php while (have_posts()) : the_post(); ?>
-				<header>
-					<h2><?php the_title(); ?></h2>
-				</header>
-				<?php the_content(); ?>
-				<?php endwhile; // End the loop ?>
-			</article>
+	<div class="profil">
+		<div class="row">
+			<div class="large-12 medium-12" role="main">
+				<article>
+					<header>
+						<h2><?php the_title(); ?></h2>
+					</header>
+					<?php the_content(); ?>
+				</article>
+			</div>
 		</div>
 	</div>
-</div>
+<?php endwhile; // End the loop ?>
 
 <?php get_footer(); ?>
