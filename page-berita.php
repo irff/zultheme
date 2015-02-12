bb6081e78fc8e8fad1b6ffb6c1bf7e4c24f7bdde
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-02-12 19:07:55 +0700

    Scaffolding news page

--- page-berita.php
+++ page-berita.php
@@ -19,127 +19,51 @@ Template Name: Berita
 
 <?php include('navigation.php'); ?>
 
-<!-- 
-<nav class="berita">
-	<div class="row">
-		<div class="large-12 columns">
-			<ul>
-				<li>Foto</li>
-				<li>Video</li>
-				<li>Media</li>
-				<li>Rilis</li>
-			</ul>
-		</div>
-	</div>	
-</nav>
- -->
- 
+
 <div class="berita thumbs">
-	<div class="row" data-equalizer>
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/1.jpg" alt="">
-					<figcaption>
-						<h3>Ketua MPR: Kisruh di DPR Selesai Dalam Dua Pekan Kedepan</h3>					
-					</figcaption>
-				</figure>				
+		<?php
+		$counter = 0;
+		$lastposts = get_posts();
+		foreach ( $lastposts as $post ) : setup_postdata( $post ); ?>
+			
+			<?php if($counter % 3 == 0): ?>
+				<div class="row" data-equalizer>
+			<?php endif; ?>
+			
+			<div class="large-4 columns">
+				<div class="entry" data-equalizer-watch>
+					<figure>
+						<img src="<?php echo get_template_directory_uri(); ?>/img/berita/1.jpg" alt="">
+						<figcaption>
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						</figcaption>
+					</figure>				
+				</div>
 			</div>
-		</div>
+			<?php if($counter % 3 == 2): ?>
+				</div>
+			<?php endif; ?>
+		<?php
+		$counter++;
+		endforeach; 
+		wp_reset_postdata(); ?>
 
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/2.jpg" alt="">
-					<figcaption>
-						<h3>Menhut Buka Pameran "The 6th IndoGreen Forestry Expo 2014"</h3>					
-					</figcaption>
-				</figure>				
+		<?php if($counter % 3 == 1): ?>
+				<div class="large-4 columns">
+					&nbsp;
+				</div>
+				<div class="large-4 columns">
+					&nbsp;
+				</div>
 			</div>
-		</div>
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/3.jpg" alt="">
-					<figcaption>
-						<h3>Menhut Terima Tiger Champion Award</h3>					
-					</figcaption>
-				</figure>				
-			</div>
-		</div>
-	</div>
+		<?php endif; ?>
 
-	<div class="row" data-equalizer>
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/4.jpg" alt="">
-					<figcaption>
-						<h3>Ketua MPR: Kisruh di DPR Selesai Dalam Dua Pekan Kedepan</h3>					
-					</figcaption>
-				</figure>				
+		<?php if($counter % 3 == 2): ?>
+				<div class="large-4 columns">
+					&nbsp;
+				</div>
 			</div>
-		</div>
-
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/5.jpg" alt="">
-					<figcaption>
-						<h3>Menhut Buka Pameran "The 6th IndoGreen Forestry Expo 2014"</h3>					
-					</figcaption>
-				</figure>				
-			</div>
-		</div>
-
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/6.jpg" alt="">
-					<figcaption>
-						<h3>Menhut Terima Tiger Champion Award</h3>					
-					</figcaption>
-				</figure>				
-			</div>
-		</div>
-	</div>
-
-	<div class="row" data-equalizer>
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/7.jpg" alt="">
-					<figcaption>
-						<h3>Ketua MPR: Kisruh di DPR Selesai Dalam Dua Pekan Kedepan</h3>					
-					</figcaption>
-				</figure>				
-			</div>
-		</div>
-
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/8.jpg" alt="">
-					<figcaption>
-						<h3>Menhut Buka Pameran "The 6th IndoGreen Forestry Expo 2014"</h3>					
-					</figcaption>
-				</figure>				
-			</div>
-		</div>
-
-		<div class="large-4 columns">
-			<div class="entry" data-equalizer-watch>
-				<figure>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/berita/9.jpg" alt="">
-					<figcaption>
-						<h3>Menhut Terima Tiger Champion Award</h3>					
-					</figcaption>
-				</figure>				
-			</div>
-		</div>
-	</div>
-	
-
+		<?php endif; ?>
 	<div class="selengkapnya">
 		<a href="" class="button">Selengkapnya</a>	
 	</div>
