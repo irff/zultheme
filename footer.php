496ef05ec8684e39a997b4ffd085e3f39eaa7e34
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-02-20 18:27:24 +0700

    Move tweet to the center

--- footer.php
+++ footer.php
@@ -1,23 +1,9 @@
-	<?php
-		require_once('tweet-php-master/TweetPHP.php');
-
-		$TweetPHP = new TweetPHP(array(
-			'consumer_key'          => 'GRIbTsuGnVZADvzu0KKR1DjM1',
-			'consumer_secret'       => '5Yx5ZmKJrwBlPFFyGVDhhcBn6QICQpz1fx7ASl0vX2xCc38b1U',
-			'access_token'          => '187520592-9p8QluoU2EapHVjOMJIgJ2MeWL9YuZkrwhGOUmx4',
-			'access_token_secret'   => 'RQzDMz3BhGCpN8hHDDMrvdNCKKUoCGhC6SeKxTarPtfIJ',
-			'twitter_screen_name'   => 'ZUL_Hasan',
-			'tweets_to_display'		=> 1,
-			'tweet_wrap_open'		=> '<h6>"',
-			'tweet_wrap_close'		=> '"</h6>',
-			'meta_wrap_open'		=> '<span class="meta"><em>',
-			'meta_wrap_close'		=> '</em></span>'
-		));
-		$tweet = $TweetPHP->get_tweet_list();
-	?>
 
 	<div class="footer">
 		<div class="row">
+			<div class="large-4 medium-4 columns logo">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/zul_logo.png" alt="">
+			</div>
 			<div class="large-4 medium-4 columns issues">
 				<h4>Perjuangan Zul</h4>
 				<ul>
@@ -25,10 +11,6 @@
 					<li><a href="<?php echo esc_url(home_url('/reformasi')); ?>">Reformasi</a></li>
 					<li><a href="<?php echo esc_url(home_url('/pluralitas')); ?>">Pluralitas</a></li>
 				</ul>
-			</div>
-			<div class="large-4 medium-4 columns">
-				<h4>Tweet Zul</h4>
-				<blockquote><?php echo $tweet; ?></blockquote>
 			</div>
 			<div class="large-4 medium-4 columns social">
 				<h4>Media Sosial</h4>
--- page-front.php
+++ page-front.php
@@ -118,6 +118,33 @@ Template Name: Front Page
 	</div>
 </div>
 
+<?php
+	require_once('tweet-php-master/TweetPHP.php');
+
+	$TweetPHP = new TweetPHP(array(
+		'consumer_key'          => 'GRIbTsuGnVZADvzu0KKR1DjM1',
+		'consumer_secret'       => '5Yx5ZmKJrwBlPFFyGVDhhcBn6QICQpz1fx7ASl0vX2xCc38b1U',
+		'access_token'          => '187520592-9p8QluoU2EapHVjOMJIgJ2MeWL9YuZkrwhGOUmx4',
+		'access_token_secret'   => 'RQzDMz3BhGCpN8hHDDMrvdNCKKUoCGhC6SeKxTarPtfIJ',
+		'twitter_screen_name'   => 'ZUL_Hasan',
+		'tweets_to_display'		=> 1,
+		'tweet_wrap_open'		=> '<h3>"',
+		'tweet_wrap_close'		=> '"</h3>',
+		'meta_wrap_open'		=> '<span class="meta"><em>',
+		'meta_wrap_close'		=> '</em></span>'
+	));
+	$tweet = $TweetPHP->get_tweet_list();
+?>
+
+<div class="twitter">
+	<?php echo $tweet; ?>
+	<div class="row">
+		<div class="large-12 column">
+			<h5 class="tweet-author">Latest tweet <a href="http://twitter.com/ZUL_Hasan">@ZUL_Hasan</a></h5>			
+		</div>
+	</div>
+</div>
+
 <div class="video">
 	<div class="row">
 		<div class="large-12 columns">
